bb141e6cbbea92f545a5fba08260b22247887e3c
azamat <azamatkhabibullaevv@gmail.com>
2023-05-09 10:46:36 -0700

    Scroll top component

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,48 @@ import { Main } from './components/Main'
 import { Footer } from './components/Footer'
 import { Drawer } from './components/Drawer'
 
-export const App = () => {
+import useScrollTrigger from '@mui/material/useScrollTrigger'
+import Fab from '@mui/material/Fab'
+import Fade from '@mui/material/Fade'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import { Box } from '@mui/material'
+
+function ScrollTop(props) {
+  const { children, window } = props
+
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 500
+  })
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#home'
+    )
+    console.log(anchor)
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'start'
+      })
+    }
+  }
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 150, right: 20 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  )
+}
+
+export const App = (props) => {
   const mode = useSelector((state) => state.theme)
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
 
@@ -26,6 +67,11 @@ export const App = () => {
         <Main />
         <Footer />
         <Drawer />
+        <ScrollTop {...props}>
+          <Fab size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
       </Layout>
     </ThemeProvider>
   )
